4d0413dd7115a22b23966be0677a7787e18782ac
tolmachev.kirill.a <tolmachev.kirill.a@gmail.com>
2023-04-30 02:52:59 +0400

    progress

---
+++ Assets/Scripts/RotateTowardsCamera.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cinemachine;
+using UnityEngine;
+using Zenject;
+using Random = UnityEngine.Random;
+
+namespace Assets.Scripts
+{
+    internal class RotateTowardsCamera : MonoBehaviour
+    {
+        [SerializeField]
+        private float _minDeviation;
+
+        [SerializeField]
+        private float _maxDeviation;
+
+        [SerializeField] private Transform _target;
+
+        [Inject] private CinemachineVirtualCamera _virtualCamera;
+
+        private void Start()
+        {
+            var rotation = _virtualCamera.m_Lens.Dutch;
+            var deviation = Random.Range(_minDeviation, _maxDeviation);
+            _target.rotation = Quaternion.Euler(0f, 0f, rotation + deviation);
+        }
+    }
+}
